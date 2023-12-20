ccd1ebc2247ad64e9bc3003b27f8a89857477e2a
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2023-12-20 09:51:31 +0100

    maj index

---
+++ apiAlter.js
@@ -0,0 +1,5 @@
+async function afficherAlter() {
+    const reponse = await fetch("https://labonnealternance.apprentissage.beta.gouv.fr/api-docs/swagger.json");
+    const alter = await reponse.json();
+    console.log(alter);
+  }
