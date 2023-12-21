ee4208a51c9595ae47d4cde1b25db93139f19804
louis frederic schefer <lf.schefer@gmail.com>
2023-12-21 15:03:39 +0100

    dynamique link

--- infos-map.js
+++ infos-map.js
@@ -1,44 +1,33 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
+
 const target = document.querySelector(".container");
-console.log(target);
 
 const url = "./data/dataJob.json"
 
 async function fetchApiData() {
-    const response = await fetch(url)
-    const data = await response.json()
-    const results = data.peJobs.results
-    console.log(results[3].company);
+  const response = await fetch(url)
+  const data = await response.json()
+  const results = data.peJobs.results
+  // console.log(results[3].company);
 
-    target.insertAdjacentHTML("afterBegin",
-    ` <section id="${3}">
-    <div class="info">
-      <div class="jobInfo">
-      <a href="infos-map.html?id=${3}">${results[3].title}</a>
-      </div>
-      <div class="companyInfo">${results[3].job.description.slice(0,200)}...</div>
-      <div class="companyInfo">${results[3].place.fullAddress}</div>
-    </div>
-    <div class="logo-fav">
-      <i class="fa-solid fa-heart"></i>
-      <i class="fa-regular fa-heart"></i>
-    </div>
+  target.insertAdjacentHTML("afterBegin",
+  ` <section id="${id}">
+  <div class="info">
+  <div class="jobInfo">
+  <a href="infos-map.html?id=${id}">${results[id].title}</a>
+  </div>
+  <div class="companyInfo">${results[id].job.description.slice(0,200)}...</div>
+  <div class="companyInfo">${results[id].place.fullAddress}</div>
+  </div>
+  <div class="logo-fav">
+  <i class="fa-solid fa-heart"></i>
+  <i class="fa-regular fa-heart"></i>
+  </div>
   </section>`
   )};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.addEventListener("load", (event) => {
+  window.addEventListener("load", (event) => {
     fetchApiData();
   })
-  
